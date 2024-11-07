916dd4f0d2af0cce997f3fea537d1c59c56f9cad
Kaha-Happi-Elias <122316532+Kaha-Happi-Elias@users.noreply.github.com>
2024-11-07 17:53:03 +0100

    Add files via upload

---
+++ calculator/javascript/calculator.js
@@ -0,0 +1,9 @@
+function appendToDisplay(value){
+    document.getElementById('display').value += value;
+}
+
+/*let message = "Hello world"
+let nombre =42
+message = "Tutoriel Javascript"
+
+console.log(message + ' '+ nombre)*/
